a7b41cb7ca8edc2f8a9a8644f3c880416331a82d
Ionuț Arțăriși <ionut@artarisi.eu>
2019-10-04 18:19:32 +0100

    Calculate and return percentiles

--- landtech/web.py
+++ landtech/web.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from geojson import Point, Feature, FeatureCollection
+import numpy as np
 
 
 def create_app():
@@ -8,6 +9,28 @@ def create_app():
 
 
 app = create_app()
+
+
+def percentiles(prices, percs=None):
+    """Return a function which calculates percentiles given a list of `prices`
+
+    The returned function will take on param `n` and return the
+    percentile in which it can be found (from the list `percs`)
+    """
+    if percs is None:
+        percs = [5, 25, 75, 90, 100]
+
+    array = np.array(prices)
+    res = np.percentile(array, percs)
+
+    # return a closure which uses the computation from above (no need to
+    # recalculate percentiles)
+    def percentile(n):
+        for label, perc in zip(percs, res):
+            if n <= perc:
+                return label
+
+    return percentile
 
 
 def fetch_features():
@@ -20,10 +43,14 @@ def fetch_features():
             x, y, p = int(x), int(y), int(p)
             places.append((x, y, p))
 
+    prices = [p for x, y, p in places]
+    percentile = percentiles(prices)
+
     features = []
     for (x, y, p) in places:
         point = Point((x, y))
-        feature = Feature(geometry=point, properties={'price': p})
+        perc = percentile(p)
+        feature = Feature(geometry=point, properties={'price': p, 'percentile': perc})
         features.append(feature)
 
     feature_collection = FeatureCollection(features)
--- tests/test_web.py
+++ tests/test_web.py
@@ -14,10 +14,19 @@ def test_places(client):
     feature1 = features[0]
     assert feature1 == {
         'geometry': {'coordinates': [60, 23], 'type': 'Point'},
-        'properties': {'price': 1422640},
+        'properties': {'price': 1422640, 'percentile': 25},
         'type': 'Feature'}
     feature_1 = features[-1]
     assert feature_1 == {
         'geometry': {'coordinates': [60, 75], 'type': 'Point'},
-        'properties': {'price': 7516381},
+        'properties': {'price': 7516381, 'percentile': 75},
         'type': 'Feature'}
+
+
+def test_percentiles():
+    perc = web.percentiles([1, 1, 2, 5, 100, 5, 9, 5, 5, 10])
+    assert perc(1) == 5
+    assert perc(2) == 25
+    assert perc(5) == 75
+    assert perc(9) == 90
+    assert perc(100) == 100
